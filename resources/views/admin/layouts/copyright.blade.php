791e6726c47c34c1a82d09bf198dc44693e93069
rajandangi <rajandangi649@gmail.com>
2024-02-29 14:39:01 +1030

    Add copyright, footer, and header templates

---
+++ resources/views/admin/layouts/copyright.blade.php
@@ -0,0 +1,9 @@
+  <footer class="main-footer">
+      <div class="footer-left">
+          Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://www.rajandangi.com.np/">Rajan
+              Dangi</a>
+      </div>
+      <div class="footer-right">
+
+      </div>
+  </footer>
---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+    <!-- General JS Scripts -->
+    <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
+
+    <!-- Template JS File -->
+    <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+    </body>
+
+    </html>
