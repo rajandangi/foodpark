ab3207aa56dc72cf6769abce1c6e1958e247f043
rajandangi <rajandangi649@gmail.com>
2024-02-29 14:38:45 +1030

    Add toastr.js for dynamic notifications and update fronend login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,7 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
 @extends('frontend.layouts.master')
 
 @section('content')
-    <!--=============================
-                        BREADCRUMB START
-                    ==============================-->
+    <!-- BREADCRUMB START -->
     <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
         <div class="fp__breadcrumb_overlay">
             <div class="container">
@@ -65,14 +15,10 @@
             </div>
         </div>
     </section>
-    <!--=============================
-                        BREADCRUMB END
-                    ==============================-->
+    <!-- BREADCRUMB END -->
 
 
-    <!--=========================
-                        SIGNIN START
-                    ==========================-->
+    <!-- SIGNIN START -->
     <section class="fp__signin" style="background: url(images/login_bg.jpg);">
         <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
             <div class="container">
@@ -82,29 +28,32 @@
                             <h2>Welcome back!</h2>
                             <p>sign in to continue</p>
                             <form method="POST" action="{{ route('login') }}">
+                                @csrf
                                 <div class="row">
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput">
                                             <label>email</label>
-                                            <input type="email" placeholder="Email">
+                                            <input type="email" placeholder="Email" name="email" required autofocus
+                                                value="{{ old('email') }}">
                                         </div>
                                     </div>
+
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput">
                                             <label>password</label>
-                                            <input type="password" placeholder="Password">
+                                            <input type="password" placeholder="Password" name="password">
                                         </div>
                                     </div>
                                     <div class="col-xl-12">
                                         <div class="fp__login_imput fp__login_check_area">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="checkbox" value=""
-                                                    id="flexCheckDefault">
+                                                    id="flexCheckDefault" name="remember_me">
                                                 <label class="form-check-label" for="flexCheckDefault">
                                                     Remeber Me
                                                 </label>
                                             </div>
-                                            <a href="forgot_password.html">Forgot Password ?</a>
+                                            <a href="{{ route('password.request') }}">Forgot Password ?</a>
                                         </div>
                                     </div>
                                     <div class="col-xl-12">
@@ -115,7 +64,7 @@
                                 </div>
                             </form>
                             <p class="or"><span>or</span></p>
-                            <p class="create_account">Dont’t have an aceount ? <a href="sign_up.html">Create
+                            <p class="create_account">Dont’t have an aceount ? <a href="{{ route('register') }}">Create
                                     Account</a>
                             </p>
                         </div>
@@ -124,7 +73,5 @@
             </div>
         </div>
     </section>
-    <!--=========================
-                        SIGNIN END
-                    ==========================-->
+    <!-- SIGNIN END -->
 @endsection
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
@@ -108,10 +108,19 @@
     <script src="{{ asset('frontend/js/wow.min.js') }}"></script>
     <!-- ex zoom js -->
     <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
-
+    <!-- toastr js -->
+    <script src="{{ asset('frontend/js/toastr.min.js') }}"></script>
     <!--main/custom js-->
     <script src="{{ asset('frontend/js/main.js') }}"></script>
-
+    {{-- Show dynamic toastr notification --}}
+    <script>
+        toastr.options.progressBar = true;
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error('{{ $error }}', 'Error');
+            @endforeach
+        @endif
+    </script>
 </body>
 
 </html>
